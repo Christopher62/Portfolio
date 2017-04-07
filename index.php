efaf3989b0fe64e3a44ae9fc19a59bfa86515633
Christopher62 <christopher.dachicourt@gmail.com>
2017-04-07 10:50:15 +0200

    Portfolio

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio de Christopher D.</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <a href="#" class="fa fa-chevron-up fa-2x" aria-hidden="true" id="toTop"></a>
+    <nav class="navbar navbar-default navbar-fixed-top" id="nav1">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNav1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="myNav1">
+        <ul class="nav navbar-nav navbar-right col-md-9">
+          <li><a href="#">home</a></li>
+          <li><a href="#title1">portfolio</a></li>
+          <li><a href="#title2">about</a></li>
+          <li><a href="#title3">contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <div class="container-fluid col-md-offset-0 col-md-12 col-xs-offset-1 col-xs-10">
+      <div class="row">
+        <div id="home">
+          <p>Bienvenue sur mon portfolio, il a été créé au cours de la formation Simplon (promo #2).</p>
+          <p>Je suis apprenant en Développement Front-End.</p>
+          <p>Bonne visite !</p>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <h1 id="title1">Portfolio :</h1>
+      </div>
+    </div>
+    <div class="container-fluid col-md-offset-3 col-sm-offset-1" id="portfolio">
+      <div class="row">
+        <input type="button" class="bton" value="TOUT" id="toutbtn">
+        <input type="button" class="bton" value="PRO" id="probtn">
+        <input type="button" class="bton" value="PERSO" id="persobtn">
+        <i class="fa fa-times-circle fa-2x quit" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="container-fluid" id="Affichage">
+      <div class="row">
+        <img src="images/image1.png">
+        <img src="images/image2.png">
+        <img src="images/image3.png">
+        <img src="images/image4.png">
+        <img src="images/mot.png">
+        <img src="images/palindrome.png">
+        <img src="images/uploadsql.png">
+        <img src="images/uploadsql2.png">
+      </div>
+    </div>
+    <div class="container-fluid" id="TOUT">
+      <div class="row">
+        <img src="images/image1.png">
+        <img src="images/palindrome.png">
+        <img src="images/Cap1.png">
+        <img src="images/uploadsql2.png">
+        <p style="color:#FFF; text-align:center">Challenge en HTML & JS, Crea en BootStrap, Exo en PHP !</p>
+      </div>
+    </div>
+    <div class="container-fluid" id="PRO">
+      <div class="row">
+        <p style="color:#FFF; text-align:center">projets pendant Simplon : A Venir ! En cours : site-vitrine pour la boulangerie Déjeuner Minute...</p>
+      </div>
+    </div>
+    <div class="container-fluid" id="PERSO" style="cursor:pointer">
+      <div class="row">
+        <img id="imgHTML" src="images/persohtml.png">
+        <img id="imgJS" src="images/persojs.png">
+        <img id="imgBS" src="images/persobs.png">
+        <img id="imgPHPSQL" src="images/persophpsql.png">
+      </div>
+    </div>
+    <div class="container-fluid" id="HTMLCSS">
+      <div class="row">
+        <button type"button" class="btn btn-danger quit2">Retour</button>
+        <p><img src="images/img2.png"></p>
+        <img src="images/ex5part1.png">
+        <img src="images/ex5part2.png">
+        <img src="images/ex5part3.png">
+        <img src="images/ex5part4.png">
+        <img src="images/maquette.png">
+        <img src="images/visageessai.png">
+        <img src="images/image1.png">
+        <img src="images/image2.png">
+        <img src="images/image3.png">
+        <img src="images/image4.png">
+        <p style="color:#FFF; text-align:center">Challenge & Exo en HTML / CSS !</p>
+      </div>
+    </div>
+    <div class="container-fluid" id="JS">
+      <div class="row">
+        <button type"button" class="btn btn-danger quit2">Retour</button>
+        <p><img src="images/js-logo.png"></p>
+        <img src="images/chaud.png">
+        <img src="images/bravo.png">
+        <img src="images/mot.png">
+        <img src="images/palindrome.png">
+        <p style="color:#FFF; text-align:center">Challenge & Exo en JavaScript !</p>
+      </div>
+    </div>
+    <div class="container-fluid" id="BootStrap">
+      <div class="row">
+        <button type"button" class="btn btn-danger quit2">Retour</button>
+        <p><img src="images/bootstrap.png"></p>
+        <img src="images/Cap1.png">
+        <img src="images/Cap2.png">
+        <img src="images/Cap3.png">
+        <img src="images/Cap4.png">
+        <img src="images/Cap5.png">
+        <img src="images/Cap6.png">
+        <img src="images/Cap7.png">
+        <img src="images/Cap8.png">
+        <img src="images/Cap9.png">
+        <img src="images/Cap10.png">
+        <p style="color:#FFF; text-align:center">Reprod, crea, challenge... en BootStrap !</p>
+      </div>
+    </div>
+    <div class="container-fluid" id="PHPSQL">
+      <div class="row">
+        <button type"button" class="btn btn-danger quit2">Retour</button>
+        <p><img src="images/php.png"></p>
+        <img src="images/phpexo3.png">
+        <img src="images/phpexo3_2.png">
+        <img src="images/uploadsql.png">
+        <img src="images/uploadsql2.png">
+        <p style="color:#FFF; text-align:center">Exo en PHP !</p>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <h1 id="title2">A propos de moi :</h1>
+      </div>
+    </div>
+    <div class="container-fluid col-md-offset-0 col-md-12 col-xs-offset-1 col-xs-10" id="about">
+      <div class="row">
+        <p>Bonjour, je m'appel Christopher, je suis âgé de 20 ans et je réside à Boulogne Sur Mer.</p>
+        <p>Je suis atuellement en formation à Simplon.co Boulogne-Sur-Mer => formation de 6 mois dans le développement web.</p>
+        <p>Bilan des compétences (au 10/03/2017) :</p>
+        <div class="progress col-md-offset-4 col-md-4 col-sm-offset-2 col-sm-8">
+          <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+            <span style="color:black">HTML / CSS (60%)</span>
+          </div>
+        </div>
+        <div class="progress col-md-offset-4 col-md-4 col-sm-offset-2 col-sm-8">
+          <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%">
+            <span style="color:black">JS (55%)</span>
+          </div>
+        </div>
+        <div class="progress col-md-offset-4 col-md-4 col-sm-offset-2 col-sm-8">
+          <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%">
+            <span style="color:black">BootStrap (65%)</span>
+          </div>
+        </div>
+        <div class="progress col-md-offset-4 col-md-4 col-sm-offset-2 col-sm-8">
+          <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100" style="width: 35%">
+            <span style="color:black">PHP (35%)</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1 id="title3">Pour me joindre :</h1>
+    <div class="container-fluid col-md-12 col-sm-12 col-xs-12 text-center" id="contain">
+      <div class="row">
+      <div class="col-md-6 col-sm-6" id="contact">
+        <form action="#contain" onsubmit="return verifForm(this)" method="POST">
+          <div class="form-group">
+            <input id="name" name="name" type="text" placeholder="Nom" class="form-control" onblur="verifName(this)" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <input id="subject" name="subject" type="text" placeholder="Objet du message" class="form-control" onblur="verifSubject(this)" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <input id="email" name="email" type="text" placeholder="E-mail" class="form-control" onblur="verifMail(this)" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" id="message" name="message" placeholder="Message..." rows="5" autocomplete="off"></textarea>
+          </div>
+          <div class="form-group">
+            <div class="text-right">
+              <button type="submit" id="sub1">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col-md-6 col-sm-6" id="adress">
+        <li><i class="fa fa-map-marker" aria-hidden="true"></i> Boulogne-Sur-Mer, Haut-de-France, France</li>
+        <li><i class="fa fa-envelope" aria-hidden="true"></i> christopher.dachicourt@gmail.com</li>
+        <div id="divnoire"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40406.95430349443!2d1.5710286544264254!3d50.73042695901836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dc2c40b157a363%3A0x40af13e8163fb30!2s62200+Boulogne-sur-Mer!5e0!3m2!1sfr!2sfr!4v1489154671671" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+      </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div id="sn">
+          <a href="https://www.facebook.com/christopher.dachicourt/" target="_blank"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a>
+          <a href="https://twitter.com/ChrisSimplon2/" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/in/christopher-dachicourt-144882110/" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+          <a href="https://github.com/Christopher62/" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="text-center" id="footer">
+          <footer>
+            <p>&copy Christopher Dachicourt</p>
+            <p id="ps">Post-scriptum : Vous pouvez retourner en haut de page via le bouton fixé (flèche à droite)</p>
+          </footer>
+        </div>
+      </div>
+    </div>
+    <script src="https://use.fontawesome.com/8d66db2fbe.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,176 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {
+        $('#toTop').fadeIn(200);
+    } else {
+        $('#toTop').fadeOut(200);
+    }
+});
+$('#toTop').click(function() {
+    $('body,html').animate({
+        scrollTop : 0
+    }, 500);
+});
+$(function(){
+  $(".quit, #PERSO, #HTMLCSS, #JS, #BootStrap, #PHPSQL").hide();
+});
+$(function(){
+    $("#TOUT").hide();
+    $("#toutbtn").click(function(){
+      $("#Affichage").fadeOut();
+      $("#TOUT").fadeIn();
+      $(".quit").show();
+    });
+});
+$(function(){
+  $(".quit").click(function(){
+    $("#Affichage").fadeIn();
+    $("#TOUT").fadeOut();
+    $(this).fadeOut();
+  });
+});
+$(function(){
+    $("#PRO").hide();
+    $("#probtn").click(function(){
+      $("#Affichage").fadeOut();
+      $("#PRO").fadeIn();
+      $(".quit").show();
+    });
+});
+$(function(){
+  $(".quit").click(function(){
+    $("#Affichage").fadeIn();
+    $("#PRO").fadeOut();
+    $(this).fadeOut();
+  });
+});
+$(function(){
+    $("#PERSO").hide();
+    $("#persobtn").click(function(){
+      $("#Affichage").fadeOut();
+      $("#PERSO").fadeIn();
+      $(".quit").show();
+    });
+});
+$(function(){
+  $(".quit").click(function(){
+    $("#Affichage").fadeIn();
+    $("#PERSO").fadeOut();
+    $(this).hide();
+  });
+});
+$(function(){
+    $("#imgHTML").click(function(){
+      $("#PERSO").hide();
+      $("#HTMLCSS").fadeIn();
+      $(".quit").hide();
+      $(".quit2").show();
+    });
+});
+$(function(){
+  $(".quit2").click(function(){
+    $("#PERSO").fadeIn();
+    $(".quit").show();
+    $("#HTMLCSS").fadeOut();
+    $(this).hide();
+  });
+});
+$(function(){
+    $("#imgJS").click(function(){
+      $("#PERSO").hide();
+      $("#JS").fadeIn();
+      $(".quit").hide();
+      $(".quit2").show();
+    });
+});
+$(function(){
+  $(".quit2").click(function(){
+    $("#PERSO").fadeIn();
+    $(".quit").show();
+    $("#JS").fadeOut();
+    $(this).hide();
+  });
+});
+$(function(){
+    $("#imgBS").click(function(){
+      $("#PERSO").hide();
+      $("#BootStrap").fadeIn();
+      $(".quit").hide();
+      $(".quit2").show();
+    });
+});
+$(function(){
+  $(".quit2").click(function(){
+    $("#PERSO").fadeIn();
+    $(".quit").show();
+    $("#BootStrap").fadeOut();
+    $(this).hide();
+  });
+});
+$(function(){
+    $("#imgPHPSQL").click(function(){
+      $("#PERSO").hide();
+      $("#PHPSQL").fadeIn();
+      $(".quit").hide();
+      $(".quit2").show();
+    });
+});
+$(function(){
+  $(".quit2").click(function(){
+    $("#PERSO").fadeIn();
+    $(".quit").show();
+    $("#PHPSQL").fadeOut();
+    $(this).hide();
+  });
+});
+    function couleur(champ, erreur){
+      if(erreur)
+        champ.style.borderColor = "red";
+      else
+        champ.style.borderColor = "green";
+    }
+    function verifName(champ){
+      var regex = /^[a-zA-Z]+[a-z]{1,}$/;
+      if(!regex.test(champ.value) || champ.value == ""){
+        alert('Le prénom ne doit pas comporter de chiffre ! / Merci de compléter ce champ');
+        couleur(champ, true);
+        return false;
+      }else{
+        couleur(champ, false);
+        return true;
+      }
+    }
+    function verifSubject(champ){
+      var regex2 = /^[a-zA-Z]$/;
+      if(champ.value == ""){
+        alert("Veuillez saisair le sujet de votre message !");
+        couleur(champ, true);
+        return false;
+      }else{
+        couleur(champ, false);
+        return true;
+      }
+    }
+    function verifMail(champ){
+      var regex3 = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,3}$/;
+      if(!regex3.test(champ.value)){
+        alert("Veuillez saisir un e-mail valide !");
+        couleur(champ, true);
+        return false;
+      }else{
+        couleur(champ, false);
+        return true;
+      }
+    }
+
+    function verifForm(f){
+      var nameOk = verifName(f.name);
+      var subjectOk = verifSubject(f.subject);
+      var mailOk = verifMail(f.email);
+      if(nameOk && subjectOk && mailOk){
+        alert("Votre message a bien été envoyé !");
+        return true;
+      }else{
+        alert("Veuillez vérfier les champs de saisies en bordure rouge !");
+        return false;
+      }
+    }
