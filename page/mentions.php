c0801d8c0439880abdf1836c9600aebb0551b113
Christopher <Christopher>
2017-08-07 18:59:06 +0200

    Portfolio

--- page/mentions.php
+++ page/mentions.php
@@ -13,14 +13,17 @@
 		#mentions{
 			margin-top: 60px;
 		}
-		#mentions p{
+		.mentions-table th{
+			text-align: center;
+		}
+		.mentions-table p{
 		  padding: 0;
 		  margin: 0;
-		  font-size: 20px;
+		  font-size: 15px;
 		}
-		#mentions li{
+		.mentions-table li{
 		  list-style-type: none;
-		  font-size: 20px;
+		  font-size: 15px;
 		}
 		</style>
 	</head>
@@ -45,18 +48,37 @@
   	</nav>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-12 col-sm-12 col-xs-12" id="mentions">
-					<p><strong>Site réalisé par :</strong><br>
-					Christopher Dachicourt</p><br>
-					<p><strong>Ressources utilisées :</strong><br>
-					<li>- Google Maps > map intégrée sur le site</li>
-					<li>- Font-Awesome > icônes</li>
-					<li>- Images > Captures des projets</li>
-					<li>- Bootstrap 3 + Jquery : mise en page & animation du site</li></p>
-					<p class="text-center"><strong>FR : Traduction en cours / EN : Translation in progress !</strong></p>
+				<div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 col-xs-offset-1 col-xs-10" id="mentions">
+					<table class="table table-bordered mentions-table">
+						<tr>
+							<th>Mentions Légales</th>
+							<th>Legal Notice</th>
+						</tr>
+						<tr>
+							<td>
+								<p><strong>Site réalisé par :</strong><br>
+								Christopher Dachicourt</p><br>
+								<p><strong>Ressources utilisées :</strong><br>
+								<li>- Google Maps > map intégrée sur le site</li>
+								<li>- Font-Awesome > icônes</li>
+								<li>- Images > Captures des projets</li>
+								<li>- Bootstrap 3 + Jquery : mise en page & animation du site</li></p>
+							</td>
+							<td>
+								<p><strong>Site created by :</strong><br>
+								Christopher Dachicourt</p><br>
+								<p><strong>Resources used :</strong><br>
+								<li>- Google Maps > map integrated on the site</li>
+								<li>- Font-Awesome > icons</li>
+								<li>- Images > Projects screenshots</li>
+								<li>- Bootstrap 3 + Jquery : Layout & animation of the site</li></p>
+							</td>
+						</tr>
+					</table>
 				</div>
 			</div>
 		</div>
+		<p class="text-center"><strong>For english users : If you click on "Back", the site will change language to french !</strong></p>
 		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		  <script src="../js/bootstrap.min.js"></script>
 	</body>
